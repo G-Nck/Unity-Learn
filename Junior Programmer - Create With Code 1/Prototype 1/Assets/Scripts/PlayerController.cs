98f52b78c5d06976eaf4e2c5e19d1ee00788b251
주진우 (Benjamin) <67905493+G-Nck@users.noreply.github.com>
2023-03-08 13:32:31 +0900

    [JuniorProgrammer-CreateWithCode-Prototype1] 1-2강 4. Vector3를 사용하여 차량 전진

--- Junior Programmer - Create With Code 1/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Junior Programmer - Create With Code 1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -14,7 +14,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(0, 0, 1);
+        // WTF is 0, 0, 1 ?!
+        //transform.Translate(0, 0, 1);
+        // Oh, Forward.
+        transform.Translate(Vector3.forward);
     }
 }
