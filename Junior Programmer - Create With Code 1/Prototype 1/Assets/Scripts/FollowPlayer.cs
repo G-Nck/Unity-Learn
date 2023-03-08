9e2b911d79deb09d5b31458c324a0ff96c0287f2
주진우 (Benjamin) <67905493+G-Nck@users.noreply.github.com>
2023-03-08 14:08:53 +0900

    [JuniorProgrammer-CreateWithCode-Prototype1] 1-3강 5. LateUpdate를 사용하여 카메라를 부드럽게 움직이기

--- Junior Programmer - Create With Code 1/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Junior Programmer - Create With Code 1/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -7,6 +7,11 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
 
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +19,7 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // 이 오브젝트 위치를 플레이어 위치에서 7m 뒤 5m 위로 조정합니다.
         transform.position = player.transform.position + offset;
