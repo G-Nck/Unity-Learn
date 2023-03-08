a55d0f734c13171f80f803e053030f56ac2e0de1
주진우 (Benjamin) <67905493+G-Nck@users.noreply.github.com>
2023-03-08 14:15:03 +0900

    [JuniorProgrammer-CreateWithCode-Prototype1] 1.4강 - 1. 차량의 좌우 이동

--- Junior Programmer - Create With Code 1/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Junior Programmer - Create With Code 1/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.Jobs;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
-
+    public float turnSpeed;
     void Start()
     {
         
@@ -15,5 +15,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+
     }
 }
